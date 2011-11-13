fd7fe09096b4ef75ddc288cd06e01b95daee51a7
Start Lynn <stuart.lynn@gmail.com>
2011-11-12 23:05:44 -0600

    setting username password on event machine

--- server/server.rb
+++ server/server.rb
@@ -1,21 +1,28 @@
 require 'eventmachine'
 require 'em-http'
 require 'json'
+require 'em-hiredis'
 
-usage = "#{$0} <user> <password>"
-abort usage unless user = ARGV.shift
-abort usage unless password = ARGV.shift
+# usage = "#{$0} <user> <password>"
+abort usage unless user ="_bucketsoftears"
+abort usage unless password = "bucketsoftears1234"
 
-url = 'http://stream.twitter.com/1/statuses/sample.json'
+url = 'https://_bucketsoftears:bucketsoftears1234@stream.twitter.com/1/statuses/filter.json?track=crying'
 
 def handle_tweet(tweet)
   return unless tweet['text']
-  puts "#{tweet['user']['screen_name']}: #{tweet['text']}"
+  puts tweet.to_json
+  @pub.publish("tears", tweet.to_json)
+end
+
+def join_redis
+  redis_details =  YAML.load_file( ZooniverseLive::Application.root('../config/redis.yml'))
+  @pub = EM::Hiredis.connect("redis://:#{redis_details['password']}@#{redis_details["host"]}:#{redis_details["port"]}/#{redis_details["db"]}")
 end
 
 EventMachine.run do
-  http = EventMachine::HttpRequest.new(url).get :head => { 'Authorization' => [ user, password ] }
-
+  
+  http = EventMachine::HttpRequest.new(url).get 
   buffer = ""
 
   http.stream do |chunk|
